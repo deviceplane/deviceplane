b793594173a69750e4d2f16bd8f588a80df3e1c4
Josh Curl <josh@curl.me>
2019-06-20 14:40:26 -0700

    Allow PATCH

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -88,7 +88,7 @@ func main() {
 		Handler: handlers.CORS(
 			handlers.AllowCredentials(),
 			handlers.AllowedHeaders([]string{"Content-Type"}),
-			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "PATCH", "DELETE"}),
 			handlers.AllowedOrigins([]string{config.AllowedOrigin}),
 		)(svc),
 	}
