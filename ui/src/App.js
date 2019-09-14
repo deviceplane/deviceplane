f73a9de392e6759e9d199bedc481fe5ff692a425
Ada <ada@deviceplane.com>
2019-09-14 13:58:51 -0700

    Revert release (#259)
    * add in revert release
    
    * add in config.endpoint

--- ui/src/App.js
+++ ui/src/App.js
@@ -2756,7 +2756,7 @@ class ApplicationOverview extends Component {
               Create New Release
             </Button>
           </Pane>
-          <Releases projectName={this.props.projectName} applicationName={this.props.application.name} />
+          <Releases projectName={this.props.projectName} applicationName={this.props.application.name} history={this.props.history}/>
         </InnerCard>
       </Pane>
     );
@@ -3186,7 +3186,9 @@ class Releases extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      releases: []
+      releases: [],
+      showRelease: false,
+      selectedRelease: null
     };
   }
 
@@ -3221,9 +3223,17 @@ class Releases extends Component {
     return "-"
   }
 
+  showSelectedRelease= (release) => {
+    this.setState({
+      showRelease: true,
+      selectedRelease: release
+    });
+  }
+
   render() {
     return (
-      <Pane>
+      <React.Fragment>
+        <Pane>
         {this.state.releases && this.state.releases.length > 0 && (
           <Table>
             <Table.Head>
@@ -3234,7 +3244,7 @@ class Releases extends Component {
             </Table.Head>
             <Table.Body>
               {this.state.releases.map(release => (
-                <Table.Row key={release.id}>
+                <Table.Row key={release.id} isSelectable onSelect={() => this.showSelectedRelease(release)}>
                   <Table.TextCell flexGrow={3} flexShrink={3}>{release.id}</Table.TextCell>
                   <Table.TextCell flexGrow={2} flexShrink={2}>{this.getReleasedBy(release)}</Table.TextCell>
                   <Table.TextCell>{moment(release.createdAt).fromNow()}</Table.TextCell>
@@ -3245,10 +3255,125 @@ class Releases extends Component {
           </Table>
         )}
       </Pane>
+      <SideSheet
+        isShown={this.state.showRelease}
+        onCloseComplete={() => this.setState({ showRelease: false })}
+      >
+        <Release release={this.state.selectedRelease} projectName={this.props.projectName} applicationName={this.props.applicationName} history={this.props.history}></Release>
+      </SideSheet>
+      </React.Fragment >
     );
   }
 }
 
+class Release extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backendError: null,
+      showConfirmDialog: false
+    };
+  }
+
+  getReleasedBy = (release) => {
+    if (release) {
+      if (release.createdByUser) {
+        const memberUrl = '../../iam/members/' + release.createdByUser.id
+        return (
+          <Link color="neutral" href={memberUrl}>{release.createdByUser.firstName} {release.createdByUser.lastName}</Link>
+        )
+      } else if (release.createdByServiceAccount) {
+        const serviceAccountUrl = '../../iam/serviceaccounts/' + release.createdByServiceAccount.name
+        return (
+          <Link color="neutral" href={serviceAccountUrl}>{release.createdByServiceAccount.name}</Link>
+        )
+      }
+    }
+    return "-"
+  }
+
+  revertRelease = () => {
+    axios.post(`${config.endpoint}/projects/${this.props.projectName}/applications/${this.props.applicationName}/releases`, {
+      config: this.props.release.config
+    }, {
+      withCredentials: true
+    })
+    .then((response) => {
+      this.setState({
+        showConfirmDialog: false
+      });
+      //track('Release Created');
+      this.props.history.push(`/${this.props.projectName}/applications/${this.props.applicationName}`)
+    })
+    .catch((error) => {
+      this.setState({
+        showConfirmDialog: false
+      });
+      if (is4xx(error.response.status)) {
+        this.setState({
+          backendError: convertErrorMessage(error.response.data)
+        });
+      } else {
+        console.log(error);
+      }
+    });
+  }
+
+  render() {
+    const release = this.props.release
+    return(
+      <React.Fragment>
+        <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
+          <Pane padding={majorScale(2)}>
+            <Heading size={600}>Release / {release.id}</Heading>
+          </Pane>
+        </Pane>
+        <Pane
+          display="flex"
+          flexDirection="column"
+          margin={majorScale(2)}
+
+        >
+          {this.state.backendError && (
+            <Alert marginBottom={majorScale(2)} paddingTop={majorScale(2)} paddingBottom={majorScale(2)} intent="warning" title={this.state.backendError} />
+          )}
+          <Pane
+            display="flex"
+            flexDirection="column"
+            marginBottom={majorScale(2)}
+          >
+            <Label><strong>Released By:</strong> {this.getReleasedBy(release)}</Label>
+            <Label><strong>Started:</strong> {moment(release.createdAt).fromNow()}</Label>
+          </Pane>
+          <Pane
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            marginBottom={majorScale(2)}
+          >
+            <Heading size={600}>Config</Heading>
+            <Button appearance="primary" justifyContent="center" onClick={() => this.setState({ showConfirmDialog: true })}>Revert to this Release</Button>
+          </Pane>
+          <Editor
+            width="100%"
+            height="300px"
+            value={release.config}
+            readOnly
+          />
+        </Pane>
+        <Dialog
+          isShown={this.state.showConfirmDialog}
+          title="Revert Release"
+          onCloseComplete={() => this.setState({ showConfirmDialog: false })}
+          onConfirm={() => this.revertRelease()}
+          confirmLabel="Revert Release"
+        >
+          This will create a new release to application <strong>{this.props.applicationName}</strong> using the config from release <strong>{release.id}</strong>.
+        </Dialog>
+      </React.Fragment>
+    )
+  }
+}
 class Register extends Component {
   constructor(props) {
     super(props);
