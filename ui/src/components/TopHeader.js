c63ef86eb4789929fe79c46254c0dc8aa9867609
Denise Schannon <ada@deviceplane.com>
2019-09-15 22:26:33 -0700

    Move CreateProject, TopHeader, UserAvatar, ChangePassword, CliDownload, EditProfile, UserAccessKeys to Components

---
+++ ui/src/components/TopHeader.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './../App.css';
+import UserAvatar from './UserAvatar.js';
+import { Pane, majorScale, Heading } from 'evergreen-ui';
+import logo from '../logo.png';
+
+export default class TopHeader extends Component {
+  render() {
+    return (
+      <Pane
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        background="white"
+        borderBottom="default"
+        padding={majorScale(1)}
+      >
+        <Pane flex={1} >
+          {this.props.showLogo && <img src={logo} alt="Logo" height="30px" width="35px" />}
+        </Pane>
+        <Pane
+          display="flex"
+          justifyContent="center"
+          flex={1}
+        >
+          <Heading size={500}>{this.props.heading}</Heading>
+        </Pane>
+        <Pane
+          display="flex"
+          justifyContent="flex-end"
+          role="button"
+          cursor="pointer"
+          flex={1}
+        >
+          <UserAvatar user={this.props.user} history={this.props.history} hideSwitchProjects={this.props.hideSwitchProjects} />
+        </Pane>
+      </Pane>
+    );
+  }
+}
