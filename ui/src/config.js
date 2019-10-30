0c785580745b62f831a48abcdadfe4e6e415e281
Josh Curl <josh@curl.me>
2019-10-30 16:16:10 -0700

    Agent version 1.5.4

--- ui/src/config.js
+++ ui/src/config.js
@@ -16,7 +16,7 @@ const config = process.env.REACT_APP_ENVIRONMENT === 'development'
   : production;
 
 export default {
-  agentVersion: '1.5.3',
+  agentVersion: '1.5.4',
   cliEndpoint: 'https://cli.deviceplane.com',
   ...config
 };
