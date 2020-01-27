b595bd23b0c7434ecdb78326081bd3366098df75
Cyrus Roshan <cyrusroshan123@gmail.com>
2020-01-26 16:09:33 -0800

    Add env variables to help output.

--- cmd/deviceplane/main.go
+++ cmd/deviceplane/main.go
@@ -22,8 +22,8 @@ var (
 
 		Flags: global.ConfigFlags{
 			APIEndpoint: app.Flag("url", "API Endpoint.").Hidden().Default("https://cloud.deviceplane.com:443/api").URL(),
-			AccessKey:   app.Flag("access-key", "Access key used for authentication.").Envar("DEVICEPLANE_ACCESS_KEY").String(),
-			Project:     app.Flag("project", "Project name.").Envar("DEVICEPLANE_PROJECT").String(),
+			AccessKey:   app.Flag("access-key", "Access key used for authentication. (env: DEVICEPLANE_ACCESS_KEY)").Envar("DEVICEPLANE_ACCESS_KEY").String(),
+			Project:     app.Flag("project", "Project name. (env: DEVICEPLANE_PROJECT)").Envar("DEVICEPLANE_PROJECT").String(),
 			ConfigFile:  app.Flag("config", "Config file to use.").Default("~/.deviceplane/config").String(),
 		},
 
