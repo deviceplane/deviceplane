ed5f06710c49a3d5a6fb31fff0685046c0d1600d
Josh Curl <josh@curl.me>
2020-02-08 16:53:25 -0800

    Remove commented code

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -17,19 +17,3 @@ func New(ctx context.Context, timeout time.Duration) (*Context, func()) {
 		Context: ctx,
 	}, cancel
 }
-
-/*func (c *Context) Deadline() (deadline time.Time, ok bool) {
-	return c.Context.Deadline()
-}
-
-func (c *Context) Done() <-chan struct{} {
-	return c.Context.Done()
-}
-
-func (c *Context) Err() error {
-	return c.Context.Err()
-}
-
-func (c *Context) Value(key interface{}) interface{} {
-	return c.Context.Value(key)
-}*/
