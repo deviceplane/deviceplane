ee573d0e338ee05e492b716851efbf97d64aa96e
Cyrus Roshan <cyrusroshan123@gmail.com>
2020-02-10 12:19:12 -0800

    Self-hosted: fix SMTP dialing. (#16)
    * Fix SMTP dialing.
    
    * Add error wrapping.

--- pkg/email/smtp/smtp.go
+++ pkg/email/smtp/smtp.go
@@ -2,12 +2,14 @@ package smtp
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net/mail"
 	"net/smtp"
 	"strings"
 
 	"github.com/deviceplane/deviceplane/pkg/email"
+	"github.com/pkg/errors"
 )
 
 type Email struct {
@@ -36,38 +38,45 @@ func (e *Email) Send(request email.Request) error {
 		Address: request.ToAddress,
 	}
 
+	certPool, err := x509.SystemCertPool()
+	if err != nil {
+		return err
+	}
+
 	conn, err := tls.Dial(
 		"tcp",
 		fmt.Sprintf("%s:%d", e.Server, e.Port),
 		&tls.Config{
 			ServerName: e.Server,
-		})
+			RootCAs:    certPool,
+		},
+	)
 	if err != nil {
-		return err
+		return errors.WithMessage(err, "creating TLS connection")
 	}
 
 	c, err := smtp.NewClient(conn, e.Server)
 	if err != nil {
-		return err
+		errors.WithMessage(err, "creating a client")
 	}
 
 	if err = c.Auth(smtp.PlainAuth(
 		"", e.Username, e.Password, e.Server,
 	)); err != nil {
-		return err
+		errors.WithMessage(err, "setting auth")
 	}
 
 	if err = c.Mail(from.Address); err != nil {
-		return err
+		return errors.WithMessage(err, "setting from address")
 	}
 
 	if err = c.Rcpt(to.Address); err != nil {
-		return err
+		return errors.WithMessage(err, "setting to address")
 	}
 
 	w, err := c.Data()
 	if err != nil {
-		return err
+		return errors.WithMessage(err, "DATA command")
 	}
 
 	if _, err = w.Write([]byte(
@@ -80,12 +89,16 @@ func (e *Email) Send(request email.Request) error {
 			request.Body,
 		}, "\r\n\r\n"),
 	)); err != nil {
-		return err
+		return errors.WithMessage(err, "writing data")
 	}
 
 	if err = w.Close(); err != nil {
-		return err
+		return errors.WithMessage(err, "closing data writer")
 	}
 
-	return c.Quit()
+	err = c.Quit()
+	if err != nil {
+		return errors.WithMessage(err, "closing connection")
+	}
+	return nil
 }
