481b7f618cdd41fa2f259c3eed485050739b7c24
Cyrus Roshan <cyrusroshan123@gmail.com>
2020-03-09 13:16:11 -0700

    Log channel type with error.

--- vendor/github.com/gliderlabs/ssh/server.go
+++ vendor/github.com/gliderlabs/ssh/server.go
@@ -270,6 +270,7 @@ func (srv *Server) HandleConn(newConn net.Conn) {
 			handler = srv.ChannelHandlers["default"]
 		}
 		if handler == nil {
+			fmt.Println("nil handler for", ch.ChannelType())
 			ch.Reject(gossh.UnknownChannelType, "unsupported channel type: "+ch.ChannelType())
 			continue
 		}
