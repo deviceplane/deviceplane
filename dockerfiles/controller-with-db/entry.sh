9ea53e653b89b7064ca9f9f1a35e6acf85a3d29f
Seandon Mooy <seandon.mooy@gmail.com>
2020-08-26 22:11:37 -0700

    ignore the lost+found directory, to allow more easy mounting of the mysql datadir (with ext4 volumes)

--- dockerfiles/controller-with-db/entry.sh
+++ dockerfiles/controller-with-db/entry.sh
@@ -5,6 +5,6 @@ MYSQL_USER=deviceplane \
   MYSQL_PASSWORD=deviceplane \
   MYSQL_RANDOM_ROOT_PASSWORD=yes \
   MYSQL_DATABASE=deviceplane \
-  docker-entrypoint.sh mysqld --sql-mode="" &
+  docker-entrypoint.sh mysqld --sql-mode="" --ignore-db-dir="lost+found" &
 
 exec controller $@
